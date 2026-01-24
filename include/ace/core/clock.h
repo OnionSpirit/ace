e092b024de326dc70de5f0a69bfbd789f0ba86a3
Ivan Moskalev <MoskalevJobMail@gmail.com>
2026-01-25 02:10:03 +0300

    Clock service added

--- include/ace/core/clock.h
+++ include/ace/core/clock.h
@@ -19,8 +19,9 @@ namespace ace::core {
         std::chrono::duration<uint64_t, std::micro> _duration;
         async<> _context;
 
-        bool operator>(const clock_record& p) const {
-            return this->_duration > p._duration;
+        // NOTE: Clock record is greater if its completion time is later
+        bool operator > (const clock_record& p) const {
+            return (this-> _request_ts + this->_duration) > (p._request_ts + p._duration);
         }
     };
 
@@ -36,6 +37,8 @@ namespace ace::core {
         // TODO: Add record detach
         async<> service_yank() {
             _current_ts = std::chrono::steady_clock::now();
+            for (auto&& record : _requests.pop_batch())
+                _queue.push(std::forward<clock_record>(record));
             for (int i = 0; not _queue.empty() and i < _releases_per_yank; ++i) {
                 const auto&[_request_ts, _duration, _context] = _queue.top();
                 if (_request_ts + _duration >= _current_ts) {
@@ -47,6 +50,9 @@ namespace ace::core {
             co_return;
         }
 
+        void subscribe(clock_record&& record) { _requests.push(std::move(record)); }
+
+        nukes::dynamic::mpsc_queue<clock_record> _requests;
         std::priority_queue<clock_record, std::deque<clock_record>, std::greater<>> _queue;
         std::chrono::time_point<std::chrono::steady_clock> _current_ts;
         int _releases_per_yank {1024};

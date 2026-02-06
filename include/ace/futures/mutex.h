422e83b6e74ae868bf4a2fed12ca6bb4f800884f
moskalev-i <moskalev-i@protei-lab.ru>
2026-02-06 18:27:41 +0300

    more bits of the mutex

--- include/ace/futures/mutex.h
+++ include/ace/futures/mutex.h
@@ -12,33 +12,43 @@
 
 namespace ace::futures {
 
-    class mutex : public future_traits<mutex> {
-
-        std::atomic_flag _captured = ATOMIC_FLAG_INIT;
-
-        nukes::dynamic::mpmc_queue<async<>> _waiters {};
-
-        // friend core::resolve_service;
-
-        void notify() noexcept;
-
-        void resolve() noexcept;
+    class mutex_locker : public future_traits<mutex_locker> {
 
         struct mutex_conductor;
         friend mutex_conductor;
 
     public:
 
-        DECLARE_FUTURE(mutex)
+        DECLARE_FUTURE(mutex_locker)
         IMPORT_FUTURE_ENV
 
-        bool await_ready() override { return capture(); }
+        bool await_ready() override { return basic_capture(); }
 
         bool await_suspend(auto coroutine);
 
         static void await_resume() {}
 
-        bool capture() noexcept;
+        bool basic_capture() noexcept;
+
+        std::atomic_flag _captured = ATOMIC_FLAG_INIT;
+        mutable nukes::dynamic::mpmc_queue<async<>> _waiters {};
+    };
+
+    class mutex : protected mutex_locker {
+
+        void notify() noexcept;
+
+        void resolve() noexcept;
+
+        // friend core::resolve_service;
+
+        friend class mutex_locker;
+
+        public:
+
+        auto capture() noexcept -> const mutex_locker &;
+
+        bool try_capture() noexcept;
 
         void sync() noexcept;
     };
@@ -47,17 +57,17 @@ namespace ace::futures {
 
 //==============================- DEFINITIONS -==================================
 
-#define ACE_FUTURE_MUTEX_SPACE \
-ace::futures::mutex::
+#define ACE_FUTURE_MUTEX_LOCKER_SPACE \
+ace::futures::mutex_locker::
 
-#define ACE_FUTURE_MUTEX_MEMBER(returnT) \
-returnT ACE_FUTURE_MUTEX_SPACE
+#define ACE_FUTURE_MUTEX_LOCKER_MEMBER(returnT) \
+returnT ACE_FUTURE_MUTEX_LOCKER_SPACE
 
-struct ACE_FUTURE_MUTEX_SPACE mutex_conductor : conductor_handler_t {
+struct ACE_FUTURE_MUTEX_LOCKER_SPACE mutex_conductor : conductor_handler_t {
 
     mutex_conductor() = delete;
 
-    explicit mutex_conductor(mutex* mutex_)
+    explicit mutex_conductor(const mutex_locker* mutex_)
         : _mutex(mutex_) {};
 
     void forward(async<>&& ctx) override {
@@ -66,8 +76,31 @@ struct ACE_FUTURE_MUTEX_SPACE mutex_conductor : conductor_handler_t {
 
     ~mutex_conductor() override = default;
 
-    mutex* const _mutex;
+    const mutex_locker* _mutex;
 };
+
+ACE_FUTURE_MUTEX_LOCKER_MEMBER(bool)
+basic_capture() noexcept {
+    // NOTE: _captured.test() == false, means that mutex is free.
+    // NOTE:  _captured.test_and_set() == false, means that mutex capture succeed
+    return not (_captured.test() or _captured.test_and_set(std::memory_order_acq_rel));
+}
+
+ACE_FUTURE_MUTEX_LOCKER_MEMBER(bool)
+await_suspend(auto coroutine) {
+    coroutine.promise()._conductor = mutex_conductor{this};
+    return true;
+}
+
+#undef ACE_FUTURE_MUTEX_LOCKER_MEMBER
+#undef ACE_FUTURE_MUTEX_LOCKER_SPACE
+
+#define ACE_FUTURE_MUTEX_SPACE \
+ace::futures::mutex::
+
+#define ACE_FUTURE_MUTEX_MEMBER(returnT) \
+returnT ACE_FUTURE_MUTEX_SPACE
+
 
 ACE_FUTURE_MUTEX_MEMBER(void)
 notify() noexcept {
@@ -81,23 +114,20 @@ resolve() noexcept {
         notify();
 }
 
-ACE_FUTURE_MUTEX_MEMBER(bool)
+ACE_FUTURE_MUTEX_MEMBER(const ace::futures::mutex_locker&)
 capture() noexcept {
-    // NOTE: _captured.test() == false, means that mutex is free.
-    // NOTE:  _captured.test_and_set() == false, means that mutex capture succeed
-    return not (_captured.test() or _captured.test_and_set(std::memory_order_acq_rel));
+    return *static_cast<mutex_locker*>(this);
+}
+
+ACE_FUTURE_MUTEX_MEMBER(bool)
+try_capture() noexcept {
+    return basic_capture();
 }
 
 ACE_FUTURE_MUTEX_MEMBER(void)
 sync() noexcept {
     _captured.clear(std::memory_order_release);
     notify();
-}
-
-ACE_FUTURE_MUTEX_MEMBER(bool)
-await_suspend(auto coroutine) {
-    coroutine.promise()._conductor = mutex_conductor{this};
-    return true;
 }
 
 #undef ACE_FUTURE_MUTEX_MEMBER

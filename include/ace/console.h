b8246803313e706ed6b272f37b6f7c78763eb834
Ivan Moskalev <MoskalevJobMail@gmail.com>
2026-05-28 23:32:26 +0300

    console upgrade

--- include/ace/console.h
+++ include/ace/console.h
@@ -26,40 +26,71 @@ namespace ace {
 
         console() = default;
 
+        struct stdio_link : core::io_link {
+
+            IMPORT_IO_LINK_ENV(stdio_link);
+            IMPORT_IO_LINK_FABRICATION;
+
+            stdio_link() { _fd = -1; _is_closed = true; }
+
+            void output_action(const std::span<const char> buff) override {
+                // NOTE: Trying to get thread local runner from the dispatcher
+                auto* runner_identity = reinterpret_cast<runner_pool_t*>(core::dispatcher::get_local_runner());
+                // NOTE: If can not get slot or identity not found -> using busy behavior
+                if (core::io_hanged::command* cmd; not core::io_hanged::_command_pool.capture(cmd) or not runner_identity) [[unlikely]] {
+                    if (::write(stdout->_fileno, buff.data(), buff.size()) < 0 and core::io_hanged::fail_cb_handler)
+                        core::io_hanged::fail_cb_handler(errno);
+                }
+                // NOTE: Pushing data to slot, and setting identity for kernelic
+                else [[likely]] {
+                    cmd->_runner_identity = runner_identity;
+                    cmd->_buffer.assign(buff.begin(), buff.end());
+                    if (not core::services::kernel_controller::write(cmd, stdout->_fileno,
+                        cmd->_buffer.data(), cmd->_buffer.size(), 0) and core::io_hanged::fail_cb_handler)
+                        core::io_hanged::fail_cb_handler(EAGAIN); // Maybe EIO?
+                }
+            };
+
+            promise<int> input_action(void *buff, const std::size_t len) override {
+                co_return co_await core::read_query(stdin->_fileno, buff, len);
+            }
+
+        };
+
         // TODO: Make io_router and upgrade this field to it
-        static fs::file_link _output;
+        static stdio_link _stdio;
 
     public:
 
         [[nodiscard]] static promise<std::expected<std::string, int>> input() {
-            co_return co_await _output.read_str();
+            co_return co_await _stdio.read_str();
         }
 
         template <class... Args>
         static void println(FMT_SRC::format_string<Args...>&& fmt, Args&&... args) {
-            _output.writeln(std::forward<FMT_SRC::format_string<Args...>>(fmt), std::forward<Args>(args)...);
+            _stdio.writeln(std::forward<FMT_SRC::format_string<Args...>>(fmt), std::forward<Args>(args)...);
         }
 
         static void println(const FMT_SRC::string_view&& str) {
-            _output.writeln(std::forward<const FMT_SRC::string_view>(str));
+            _stdio.writeln(std::forward<const FMT_SRC::string_view>(str));
         }
 
         static void println() {
-            _output.writeln("");
+            _stdio.writeln("");
         }
 
         template <class... Args>
         static void print(FMT_SRC::format_string<Args...>&& fmt, Args&&... args) {
-            _output.write(std::forward<FMT_SRC::format_string<Args...>>(fmt), std::forward<Args>(args)...);
+            _stdio.write(std::forward<FMT_SRC::format_string<Args...>>(fmt), std::forward<Args>(args)...);
         }
 
         static void print(const FMT_SRC::string_view&& str) {
-            _output.write(std::forward<const FMT_SRC::string_view>(str));
+            _stdio.write(std::forward<const FMT_SRC::string_view>(str));
         }
 
     };
 
-    fs::file_link console::_output { stdout->_fileno };
+    console::stdio_link console::_stdio {};
 
 }
 
